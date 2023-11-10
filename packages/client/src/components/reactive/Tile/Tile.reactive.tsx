7c745beb4fa4ffa548ae9da1bb794e4c12481a76
Lucky4rever <snigur.pavlo@lll.kpi.ua>
2023-11-10 13:15:49 +0200

    feat: create page for fandoms

--- packages/client/src/components/reactive/Tile/Tile.reactive.tsx
+++ packages/client/src/components/reactive/Tile/Tile.reactive.tsx
@@ -1,36 +1,16 @@
 import { type TileProps } from "@utils/types/tile.type";
-import { TileLayout } from "./Tile.styled";
-import { useEffect, useRef, type ElementRef } from "react";
 
 type Props = Prettify<TileProps>;
 
-const Tile = ({id, title, description, img}: Props): JSX.Element => {
-  const tile = useRef<ElementRef<"img">>(null);
-
-  useEffect(() => {
-    tile.current?.addEventListener("click", expandImage);
-    console.log("added event listener");
-  }, []);
-
-  function expandImage(e: Event) {
-    if(tile.current !== e.target) {
-      tile.current?.classList.remove("gallery-expand");
-    }
-
-    const selectedImage = e.target as HTMLElement;
-    selectedImage?.classList.add("gallery-expand");
-  }
-
+const Tile = ({fandom, logo, ['front-image']: frontImage, ['bg-image']: bgImage}: TileProps) => {
   return (
-    // <a key={id} href={`/fandom/${id}`}>
-    //   <div>
-    //     <h1>{title}</h1>
-    //     <p>{description}</p>
-    //     <img src={img} alt={title} width={200} />
-    //   </div>
-    // </a>
-    <TileLayout ref={tile} src={img} alt={title} width={200} />
-
+    <a className="tile-link" href={`fandom/${fandom}`}>
+    <div className="tile">
+      <img className="logo" src={logo} alt="" />
+      <img className="front-image" src={frontImage} alt="" />
+      <img className="bg-image" src={bgImage} alt="" />
+    </div>
+    </a>
   );
 }
 
--- packages/client/src/utils/types/tile.type.ts
+++ packages/client/src/utils/types/tile.type.ts
@@ -1,10 +1,17 @@
-interface TileProps {
-  id: number;
-  title: string;
-  description: string;
-  img: string;
+type TileProps = 
+// {
+//   id: number;
+//   title: string;
+//   description: string;
+//   img: string;
 
-  classList?: string | string[];
-}
+//   classList?: string | string[];
+// } | 
+{
+  fandom: string;
+  logo: string;
+  "front-image": string;
+  "bg-image": string;
+};
 
 export { type TileProps };
