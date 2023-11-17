1c146fb3ce32b858a4a6e968df3f06140d766c9f
Lucky4rever <snigur.pavlo@lll.kpi.ua>
2023-11-17 23:25:53 +0200

    fix: deploying 2

--- packages/client/astro.config.mjs
+++ packages/client/astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   root: './',
   output: 'server',
-  adapter: vercel(),
   server: {
     port: 3000,
     host: '::1'
   },
-  integrations: [react()]
+  integrations: [react()],
+  adapter: vercel()
 });
